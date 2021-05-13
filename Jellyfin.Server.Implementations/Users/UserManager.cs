f7c63cd86bad25aa2aa94d093c0c8b2d77df1a7b
GermanCoding <github@germancoding.com>
2021-05-13 17:58:15 +0200

    Add option to specify user id on user creation
    Changing user id after creation is not realistically possible, so we
    need this option for custom plugins
    
    Rebased for 12.0

--- Jellyfin.Server.Implementations/Users/UserManager.cs
+++ Jellyfin.Server.Implementations/Users/UserManager.cs
@@ -1,4 +1,4 @@
-#pragma warning disable RS0030 // Do not use banned APIs
+﻿#pragma warning disable RS0030 // Do not use banned APIs
 
 using System;
 using System.Collections.Generic;
@@ -314,7 +314,7 @@ namespace Jellyfin.Server.Implementations.Users
             }
         }
 
-        internal async Task<User> CreateUserInternalAsync(string name, JellyfinDbContext dbContext)
+        internal async Task<User> CreateUserInternalAsyncWithGuid(string name, JellyfinDbContext dbContext, Guid id)
         {
             // TODO: Remove after user item data is migrated.
             var max = await dbContext.Users.AsQueryable().AnyAsync().ConfigureAwait(false)
@@ -329,14 +329,30 @@ namespace Jellyfin.Server.Implementations.Users
                 InternalId = max + 1
             };
 
+            if (!id.Equals(default))
+            {
+                user.Id = id;
+            }
+
             user.AddDefaultPermissions();
             user.AddDefaultPreferences();
 
             return user;
         }
 
+        internal async Task<User> CreateUserInternalAsync(string name, JellyfinDbContext dbContext)
+        {
+            return await CreateUserInternalAsyncWithGuid(name, dbContext, Guid.Empty).ConfigureAwait(false);
+        }
+
         /// <inheritdoc/>
         public async Task<User> CreateUserAsync(string name)
+        {
+            return await CreateUserAsync(name, Guid.Empty).ConfigureAwait(false);
+        }
+
+        /// <inheritdoc/>
+        public async Task<User> CreateUserAsync(string name, Guid id)
         {
             ThrowIfInvalidUsername(name);
 
@@ -356,7 +372,7 @@ namespace Jellyfin.Server.Implementations.Users
                 }
 #pragma warning restore CA1862 // Use the 'StringComparison' method overloads to perform case-insensitive string comparisons
 
-                newUser = await CreateUserInternalAsync(name, dbContext).ConfigureAwait(false);
+                newUser = await CreateUserInternalAsyncWithGuid(name, dbContext, id).ConfigureAwait(false);
 
                 dbContext.Users.Add(newUser);
                 await dbContext.SaveChangesAsync().ConfigureAwait(false);
