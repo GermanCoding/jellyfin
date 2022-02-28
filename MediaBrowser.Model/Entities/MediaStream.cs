6e1d58854bc1b5a8e7a2915b86581acfe9670bfa
GermanCoding <github@germancoding.com>
2022-02-28 03:19:19 +0100

    Use native display language for sound/subtitle languages
    Just overall nicer and more consistent.

--- MediaBrowser.Model/Entities/MediaStream.cs
+++ MediaBrowser.Model/Entities/MediaStream.cs
@@ -238,7 +238,13 @@ namespace MediaBrowser.Model.Entities
                             string fullLanguage = CultureInfo
                                 .GetCultures(CultureTypes.NeutralCultures)
                                 .FirstOrDefault(r => r.ThreeLetterISOLanguageName.Equals(Language, StringComparison.OrdinalIgnoreCase))
-                                ?.DisplayName;
+                                ?.NativeName;
+                            // Fix "ger" for German
+                            if (string.Equals(Language, "ger", StringComparison.OrdinalIgnoreCase))
+                            {
+                                    fullLanguage = CultureInfo.GetCultureInfo("deu").NativeName;
+                            }
+
                             attributes.Add(StringHelper.FirstToUpper(fullLanguage ?? Language));
                         }
 
@@ -338,7 +344,7 @@ namespace MediaBrowser.Model.Entities
                             string fullLanguage = CultureInfo
                                 .GetCultures(CultureTypes.NeutralCultures)
                                 .FirstOrDefault(r => r.ThreeLetterISOLanguageName.Equals(Language, StringComparison.OrdinalIgnoreCase))
-                                ?.DisplayName;
+                                ?.NativeName;
                             attributes.Add(StringHelper.FirstToUpper(fullLanguage ?? Language));
                         }
                         else
